761bbeebe2e6bd0f1cce7cde5880840cbb1644ef
Will Refvem <wrefvem@cloudbees.com>
2018-02-06 15:33:27 -0800

    updated url

--- app/todos/urls.py
+++ app/todos/urls.py
@@ -4,7 +4,7 @@ from todos import views
 
 urlpatterns = {
     url(r'^$', views.TodoList.as_view(), name='list'),
-    url(r'^/detail/(?P<pk>[0-9]+)/$', views.TodoDetail.as_view(), name='details'),
+    url(r'^detail/(?P<pk>[0-9]+)/$', views.TodoDetail.as_view(), name='details'),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
